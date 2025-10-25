815f29597403825b7ab981d6337d5b62b61225f0
Eberechi Uchechi Grace <uchechieberechi@gmail.com>
2025-10-25 04:37:48 +0100

    Updated contact page

--- lib/pages/contact_page.dart
+++ lib/pages/contact_page.dart
@@ -1,7 +1,15 @@
 import 'package:flutter/material.dart';
+import '../widgets/contact_item.dart';
 
-class ContactPage extends StatelessWidget {
+class ContactPage extends StatefulWidget {
   const ContactPage({super.key});
+
+  @override
+  State<ContactPage> createState() => _ContactPageState();
+}
+
+class _ContactPageState extends State<ContactPage> {
+ 
 
   @override
   Widget build(BuildContext context) {
@@ -17,59 +25,11 @@ class ContactPage extends StatelessWidget {
         padding: EdgeInsets.all(16),
         children: [
           Card(
-            child: ListTile(
-              onTap: () {
-                showModalBottomSheet(
-                  context: context,
-                  builder: (context) {
-                    return Padding(
-                      padding: const EdgeInsets.all(16),
-                      child: ListView(
-                        //crossAxisAlignment: CrossAxisAlignment.stretch,
-                        children: [
-                          Text(
-                            "String of Life Hospital",
-                            style: TextStyle(fontWeight: FontWeight.bold),
-                            textAlign: TextAlign.center,
-                          ),
-                          Text(
-                            "Naze by Poly Junction,Owerri",
-                            textAlign: TextAlign.center,
-                          ),
-                          Row(
-                            spacing: 24,
-                            mainAxisAlignment: MainAxisAlignment.center,
-                            children: [
-                              IconButton(onPressed: () {}, icon:  Icon(Icons.call, color: Colors.green, size: 40)),
-                              Icon(Icons.message, color: Colors.blue, size: 40),
-                            ],
-                          ),
-                          SizedBox(height: 24),
-                          Text("History", textAlign: TextAlign.left),
-                          Text("No History Yet!", textAlign: TextAlign.right),
-                        ],
-                      ),
-                    );
-                  },
-                );
-              },
-              leading: Container(
-                padding: EdgeInsets.all(10),
-                decoration: BoxDecoration(
-                  color: Colors.lightBlueAccent.shade100,
-                  borderRadius: BorderRadius.circular(8),
-                ),
-                child: Icon(Icons.add_box_outlined),
-              ),
-              title: Text(
-                "Spring of Life Clinic",
-                style: TextStyle(fontWeight: FontWeight.bold),
-              ),
-              subtitle: Text("14th November, 2025"),
-            ),
+            child: Contactitem(),
           ),
         ],
       ),
     );
   }
 }
+
